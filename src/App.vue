988d7eb44986908f56edcfefd37158c1ac726e77
Petar Trenchev <petar.trenchev@valtech.com>
2025-05-22 10:17:10 +0200

    debug

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,27 @@
   <DefaultLayout>
     <RouterView />
     <pre>
-      {{ isLoggedIn() }}
+      {{ currentUser }}
     </pre>
   </DefaultLayout>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import TheHeader from '@/shared/components/TheHeader/TheHeader.vue';
 import { account } from '@/shared/utils/api.ts';
 
+const currentUser = ref()
+
 const isLoggedIn = async () => {
-  return await account.get()
+  try {
+    currentUser.value = await account.get()
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+isLoggedIn()
 </script>
