712fa6c6c09a0bf47e39493becd47b50ed2b1899
Petar Trenchev <petar.trenchev@valtech.com>
2025-05-22 10:40:01 +0200

    use auth store to avoid multiple calls

--- src/router/middleware.ts
+++ src/router/middleware.ts
@@ -1,28 +1,26 @@
 import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
 
 import { useFeatureFlags } from '@/config/feature-flags.ts';
+import { useAuthStore } from '@/features/Login/stores/useAuthStore.ts';
 import { router } from '@/router/index.ts'
 import { account } from '@/shared/utils/api.ts';
 
 router.beforeEach(async (to: RouteLocationNormalized, _from: RouteLocationNormalized, next: NavigationGuardNext) => {
+  const authStore = useAuthStore();
   const { isFeatureEnabled } = useFeatureFlags()
 
   console.log('start middleware');
 
-  if (to.meta.requiresAuth) {
-    try {
-      await account.get();
-
-      return next()
-    } catch {
-      return next('/')
-    }
+  if (to.meta.requiresAuth && !authStore.isLoggedIn) {
+    console.log('requires auth');
+    return next('/')
   }
 
   if (to.path === '/') {
     console.log('login page');
     try {
-      await account.get();
+      const user = await account.get();
+      authStore.setCurrentUser(user);
 
       return next('/dashboard')
     } catch {
