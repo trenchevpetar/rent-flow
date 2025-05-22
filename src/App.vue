9f87db18643036dbf8526d2aa15976c1b2769b05
Petar Trenchev <petar.trenchev@valtech.com>
2025-05-22 10:11:20 +0200

    debug

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,18 @@
   <TheHeader />
   <DefaultLayout>
     <RouterView />
+    <pre>
+      {{ isLoggedIn }}
+    </pre>
   </DefaultLayout>
 </template>
 
 <script setup lang="ts">
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import TheHeader from '@/shared/components/TheHeader/TheHeader.vue';
+import { account } from '@/shared/utils/api.ts';
+
+const isLoggedIn = async () => {
+  return await account.get()
+}
 </script>
