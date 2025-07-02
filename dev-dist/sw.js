9a2f0bb13d9584183838b8b784bd4b1744c71bab
Petar Trenchev (Valtech.com) <petar.trenchev@valtech.com>
2025-07-02 13:43:08 +0200

    categories v1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g1a6ikvm60g"
+    "revision": "0.cpage7l5sr"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/features/Property/AddProperty/components/EditExpenseForm.vue
+++ src/features/Property/AddProperty/components/EditExpenseForm.vue
@@ -6,7 +6,7 @@
     <fieldset class="fieldset w-full">
       <InputSelect
         v-model="formValues.category"
-        :items="expenseTypes"
+        :items="formattedCategories"
         default-value="Expense type"
         label="Expense categories"
       />
@@ -43,10 +43,14 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
+import { useQuery } from '@tanstack/vue-query';
+import { computed, ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
 
-import { expenseCategoriesForSelect } from '@/features/Property/AddProperty/constants/expense.category.ts';
+import { useAuthStore } from '@/features/Login/stores/useAuthStore.ts';
 import type { Expense, UpdatableExpense } from '@/features/Property/AddProperty/types/expense.types.ts';
+import { refetchCategories, resolveCategoryLabels } from '@/features/Property/Categories/services/category.service.ts';
+import type { Category } from '@/features/Property/Categories/types/category.type.ts';
 import InputDate from '@/shared/components/InputDate/InputDate.vue';
 import InputField from '@/shared/components/InputField/InputField.vue';
 import InputSelect from '@/shared/components/InputSelect/InputSelect.vue';
@@ -56,7 +60,12 @@ import TheSpinner from '@/shared/components/TheSpinner/TheSpinner.vue';
 const props = defineProps<{
   expense: Expense;
 }>();
-
+const authStore = useAuthStore()
+const route = useRoute()
+const propertyId = computed(() => route.params.id as string)
+const userId = computed(() => authStore.currentUser?.$id || '')
+const categories = ref()
+const formattedCategories = ref()
 const emit = defineEmits(['on-edit-confirm'])
 const formValues = ref<UpdatableExpense>({
   propertyId: props.expense.propertyId || '',
@@ -67,7 +76,30 @@ const formValues = ref<UpdatableExpense>({
   isPaid: props.expense.isPaid || false
 })
 const isLoading = ref(false);
-const expenseTypes = computed(() => expenseCategoriesForSelect())
+
+const propertyCategoriesQuery = useQuery({
+  queryKey: ['property-categories', propertyId.value],
+  queryFn: () => refetchCategories(propertyId.value),
+  enabled: computed(() => !!propertyId.value),
+  staleTime: 60 * 1000, // 1 min stale
+  retry: 3
+})
+
+watch(
+  () => propertyCategoriesQuery.data.value?.categoryIds,
+  async (newCategoryIds) => {
+    if (newCategoryIds) {
+      categories.value = await resolveCategoryLabels(userId.value, newCategoryIds)
+      formattedCategories.value = categories.value.map((category: Category) => ({
+        value: category.id,
+        label: category.label
+      }))
+    }
+  },
+  {
+    immediate: true
+  }
+)
 
 const onEditExpense = () => emit('on-edit-confirm', {
   ...props.expense,
